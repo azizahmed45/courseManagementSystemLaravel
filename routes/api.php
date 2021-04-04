e9a06401d1152b23473fb42675613af17bce1fe6
Aziz Ahmed <azizahmedsabuj@gmail.com>
2021-04-04 15:02:14 +0600

    Test api 2

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/sendSms', function(Request $request){
+Route::post('/sendData', function(Request $request){
+    \Illuminate\Support\Facades\Log::info($request);
     return $request;
 });
